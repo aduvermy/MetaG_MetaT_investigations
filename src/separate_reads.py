fe314e115be451dc282c6cff8285ae959b165fc1
Ubuntu <ubuntu@machineb170dd53-ea33-4000-9cc7-d4ae953982db.in2p3.fr>
2021-03-25 13:11:02 +0000

    modif arg parse

--- src/separate_reads.py
+++ src/separate_reads.py
@@ -1,17 +1,34 @@
 #!/usr/bin/env python3
 
 import os
-import sys
+import argparse
+from Bio import SeqIO
+import pandas as pd
 
-basename = os.path.basename(sys.argv[1])
+
+parser = argparse.ArgumentParser(description='Check my input')
+
+parser.add_argument('--fastq', '-f', type=str,
+                    help='Path of fastq file. For exemple :  "sequence_alignment/mysample.fastq"')
+parser.add_argument('--output', '-o', type=str,
+                    help='output directory :  "../../"')
+
+args = parser.parse_args()
+
+
+basename = os.path.basename(args.fastq)
 splited_base = basename.split(".")
 base = splited_base[0]
+print(f"output read 1: {args.output}{base}_1.fastq \n" )
+print(f"output read 2: {args.output}{base}_2.fastq \n" )
 
 
-read_1 = open(base+"_1.fastq","w")
-read_2 = open(base+"_2.fastq","w")
+read_1 = open(f"{args.output}{base}_1.fastq","w")
+read_2 = open(f"{args.output}{base}_2.fastq","w")
 
-with open(sys.argv[1], "r") as f:
+
+
+with open(args.fastq, "r") as f:
 	for line in f:
 		if "@SRR" == line[:3] or "+SRR" == line[:3]:
 			splited = line.split(" ")
